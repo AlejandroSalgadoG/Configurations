8c79076e6b611c13e41d39044a66e9c88e832c3c
Alejandro Salgado G <asalgad2@eafit.edu.co>
2017-11-29 10:28:46 -0500

    new version of ServerConfig for ubuntu server

--- Bash/ServerConfig.sh
+++ Bash/ServerConfig.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 cd
-sudo yum install -y git
+sudo apt-get install -y git
 
 git clone https://github.com/AlejandroSalgadoG/Vim.git
 git clone https://github.com/AlejandroSalgadoG/Bash.git
@@ -9,11 +9,12 @@ git clone https://github.com/AlejandroSalgadoG/Bash.git
 echo "source ~/Bash/bashrc" > .bashrc
 echo "source ~/Bash/bash_aliases" > .bash_aliases
 
-sed -i "8,15d" Bash/bash_aliases
-sed -i "11,12d" Bash/bash_aliases
-sed -i "12,13d" Bash/bash_aliases
-sed -i "13,16d" Bash/bash_aliases
-sed -i "15,17d" Bash/bash_aliases
+sed -i "6,13d" Bash/bash_aliases
+sed -i "9,10d" Bash/bash_aliases
+sed -i "10,11d" Bash/bash_aliases
+sed -i "11,14d" Bash/bash_aliases
+sed -i "15,17d" Bash/bash_aliases #end of aliases
+sed -i "17,50d" Bash/bash_aliases
 sed -i "21,118d" Bash/bash_aliases
 sed -i "29,68d" Bash/bash_aliases
 sed -i '38,$d' Bash/bash_aliases
