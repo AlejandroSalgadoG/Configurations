e5728d54490357219b07f683d73f0270dc4babd0
Alejandro Salgado G <asalgad2@eafit.edu.co>
2017-11-29 11:19:06 -0500

    Updating SetConfig script to accept new configurations

--- Bash/SetConfig.sh
+++ Bash/SetConfig.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 echo -n "Copying gitconfig..."
-cp ~/Github/Bash/gitconfig ~/.gitconfig
+cp ~/Github/Configurations/Bash/gitconfig ~/.gitconfig
 echo done
 
 echo -n "Configuring bashrc..."
 cat << EOF > ~/.bashrc
-source ~/Github/Bash/bashrc
+source ~/Github/Configurations/Bash/bashrc
 EOF
 echo done
 
 echo -n "Configuring bash_aliases..."
 cat << EOF > ~/.bash_aliases
-source ~/Github/Bash/bash_aliases
+source ~/Github/Configurations/Bash/bash_aliases
 EOF
 echo done
 
 echo -n "Configuring vimrc..."
 cat << EOF > ~/.vimrc
-so ~/Github/Vim/vimrc
+so ~/Github/Configurations/Vim/vimrc
 EOF
 echo done
 
