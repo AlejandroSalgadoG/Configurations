3b07133aa6e42a7da3cb82be54e43a0dbcf70233
Alejandro Salgado G <asalgad2@eafit.edu.co>
2017-11-29 10:30:49 -0500

    new version for the new bash_aliases

--- Bash/ServerConfig.sh
+++ Bash/ServerConfig.sh
@@ -14,19 +14,19 @@ git clone https://github.com/AlejandroSalgadoG/Bash.git
 echo "source ~/Bash/bashrc" > .bashrc
 echo "source ~/Bash/bash_aliases" > .bash_aliases
 
-sed -i "6,13d" Bash/bash_aliases
-sed -i "9,10d" Bash/bash_aliases
-sed -i "10,11d" Bash/bash_aliases
-sed -i "11,14d" Bash/bash_aliases
-sed -i "15,17d" Bash/bash_aliases #end of aliases
-sed -i "17,50d" Bash/bash_aliases
-sed -i "21,118d" Bash/bash_aliases
-sed -i "29,68d" Bash/bash_aliases
-sed -i '38,$d' Bash/bash_aliases
+sed -i "10,12d" Bash/bash_aliases
+sed -i "14d" Bash/bash_aliases
+sed -i "15,21d" Bash/bash_aliases
+sed -i "19,21d" Bash/bash_aliases
+sed -i "21,24d" Bash/bash_aliases
+sed -i "21,54d" Bash/bash_aliases
+sed -i "26,44d" Bash/bash_aliases
+sed -i "26,49d" Bash/bash_aliases
+sed -i "34,73d" Bash/bash_aliases
 
 sed -i "s&alejandro/Github&$USER&g" Bash/bash_aliases
 
 cp Vim/vimrc .vimrc
 
 sed -i "s&alejandro/Github&$USER&g" .vimrc
-sed -i "s&Github&&g" Vim/VimPlugin/Commands.vim
+sed -i "s&Github&&g" Vim/VimPlugin/Main.vim
