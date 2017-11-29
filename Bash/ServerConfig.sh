7f06a1f18b577fef225e3070281f677b44822f3c
Alejandro Salgado G <asalgad2@eafit.edu.co>
2017-11-29 10:33:23 -0500

    new server configuration because new bash_aliases

--- Bash/ServerConfig.sh
+++ Bash/ServerConfig.sh
@@ -14,13 +14,12 @@ git clone https://github.com/AlejandroSalgadoG/Bash.git
 echo "source ~/Bash/bashrc" > .bashrc
 echo "source ~/Bash/bash_aliases" > .bash_aliases
 
-sed -i "10,12d" Bash/bash_aliases
-sed -i "14d" Bash/bash_aliases
-sed -i "15,21d" Bash/bash_aliases
-sed -i "24,26d" Bash/bash_aliases
-sed -i "26,63d" Bash/bash_aliases
-sed -i "30,72d" Bash/bash_aliases
-sed -i "38,77d" Bash/bash_aliases
+sed -i "10,12d" Bash/bash_aliases #i3 aliases
+sed -i "14d" Bash/bash_aliases    #open
+sed -i "15,21d" Bash/bash_aliases #deb, msg, rf, rm -i, moo, put bin
+sed -i "23,25d" Bash/bash_aliases #doge
+sed -i "26,86d" Bash/bash_aliases #search, slurm, cs, usb
+sed -i "33,72d" Bash/bash_aliases #multi
 
 sed -i "7d" Bash/bashrc
 sed -i '7iFRED="\\[\\033[31m\\]" # foreground red' Bash/bashrc
