324de5b0d08c91251495218f1b36184490cd5954
Alejandro Salgado G <asalgad2@eafit.edu.co>
2017-11-29 11:07:15 -0500

    changing PS1 in bash ans Server scripts

--- Bash/ServerConfig.sh
+++ Bash/ServerConfig.sh
@@ -16,16 +16,14 @@ echo "source ~/Bash/bash_aliases" > .bash_aliases
 
 sed -i "10,12d" Bash/bash_aliases #i3 aliases
 sed -i "14d" Bash/bash_aliases    #open
-sed -i "15,21d" Bash/bash_aliases #deb, msg, rf, rm -i, moo, put bin
+sed -i "15d" Bash/bash_aliases    #open
 sed -i "23,25d" Bash/bash_aliases #doge
-sed -i "26,86d" Bash/bash_aliases #search, slurm, cs, usb
-sed -i "33,72d" Bash/bash_aliases #multi
+sed -i "64,124d" Bash/bash_aliases #search, slurm, cs, usb
+sed -i "72,111d" Bash/bash_aliases #multi
 
-sed -i "7d" Bash/bashrc
-sed -i '7iFRED="\\[\\033[31m\\]" # foreground red' Bash/bashrc
-sed -i "10d" Bash/bashrc
-sed -i '10iPS1="${FWHI}\\T ${HC}${FRED}you are in ${FWHI}\\h ${FRED}on \\w $FYEL-> $RS"' Bash/bashrc
-sed -i "23d" Bash/bashrc
+sed -i "16d" Bash/bashrc
+sed -i '16iPS1="${HC}${WHI}\\T ${RED} you are in \\h \\w $YEL-> $RS"' Bash/bashrc
+sed -i "24d" Bash/bashrc
 
 sed -i "s/Github\///g" Bash/bash_aliases
 
