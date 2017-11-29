ae716ed6587e5c32d7b2fc66958f341de48c6dcd
Alejandro Salgado G <asalgad2@eafit.edu.co>
2017-11-29 11:07:55 -0500

    removing alias modification in the server script

--- Bash/ServerConfig.sh
+++ Bash/ServerConfig.sh
@@ -14,13 +14,6 @@ git clone https://github.com/AlejandroSalgadoG/Bash.git
 echo "source ~/Bash/bashrc" > .bashrc
 echo "source ~/Bash/bash_aliases" > .bash_aliases
 
-sed -i "10,12d" Bash/bash_aliases #i3 aliases
-sed -i "14d" Bash/bash_aliases    #open
-sed -i "15d" Bash/bash_aliases    #open
-sed -i "23,25d" Bash/bash_aliases #doge
-sed -i "64,124d" Bash/bash_aliases #search, slurm, cs, usb
-sed -i "72,111d" Bash/bash_aliases #multi
-
 sed -i "16d" Bash/bashrc
 sed -i '16iPS1="${HC}${WHI}\\T ${RED} you are in ${WHI}\\h ${RED}\\w $YEL-> $RS"' Bash/bashrc
 sed -i "24d" Bash/bashrc
